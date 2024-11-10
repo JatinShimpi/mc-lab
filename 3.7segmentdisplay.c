b57ada9dcf62094a86a01089283b62a23065942c
JatinShimpi <shimpijatin184@gmail.com>
2024-11-10 16:31:10 +0530

    added hex codes from a to f in 7 segment

--- 3.7segmentdisplay.c
+++ 3.7segmentdisplay.c
@@ -1,7 +1,7 @@
 #include <reg51.h>
 #define SEGMENT_PORT P2 // Define the port connected to the 7-segment display
 // Segment values for digits 0 to 9
-unsigned char segment_code[10] = {0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F};
+unsigned char segment_code[16] = {0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F,0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71};
 void delay(unsigned int ms)
  {
  unsigned int i, j;
@@ -14,7 +14,7 @@ void main() {
  unsigned char i;
  while (1)
 {
- for (i = 0; i < 10; i++)
+ for (i = 0; i < 16; i++)
  {
  SEGMENT_PORT = segment_code[i]; // Send the segment code to the port
  delay(1000); // Delay for 1 second
